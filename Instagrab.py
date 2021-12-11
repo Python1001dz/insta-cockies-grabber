a470a139e7d449a130e4bd5af882504e8d8f8ad4
Python1001dz <93930540+Python1001dz@users.noreply.github.com>
2021-12-11 10:11:00 +0100

    Update Instagrab.py

--- Instagrab.py
+++ Instagrab.py
@@ -9,8 +9,8 @@ login_url = 'https://www.instagram.com/accounts/login/ajax/'
 time = int(datetime.now().timestamp())
 
 payload = {
-    'username': '<mr1_naili>',
-    'enc_password': f'#PWD_INSTAGRAM_BROWSER:0:{time}:<140911@Mm>',
+    'username': '<USERNAME HERE>',
+    'enc_password': f'#PWD_INSTAGRAM_BROWSER:0:{time}:<PASSWORD HERE>',
     'queryParams': {},
     'optIntoOneTap': 'false'
 }
